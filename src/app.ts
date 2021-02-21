7faf551f997ffd32acc1681a70e54720f5e65475
Hung Nguyen <nthung.vlvn@gmail.com>
2021-02-21 21:33:12 +0700

    string, boolean, number

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
-function add(num1: number, num2: number) {
+function add(num1: number, num2: number, showResult: boolean, phrase: string) {
   // if (typeof num1 !== 'number' || typeof num2 !== 'number') {
   //   throw new Error('Invalid input');
   // }
-  return num1 + num2;
+  if (showResult) {
+    console.log(phrase + (num1 + num2));
+  } else {
+    return num1 + num2;
+  }
 }
 const num1 = 5;
 const num2 = 3.3;
+const printResult = true;
+const resultPhrase = 'The result is ';
 
-const result = add(num1, num2)
-console.log(result);
+add(num1, num2, printResult, resultPhrase);
