67a6c5f1d64f0d4c8245e56758205752913f7f4e
Hung Nguyen <nthung.vlvn@gmail.com>
2021-02-21 21:23:36 +0700

    ts types vs js types

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 function add(num1: number, num2: number) {
+  // if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+  //   throw new Error('Invalid input');
+  // }
   return num1 + num2;
 }
 const num1 = 5;
