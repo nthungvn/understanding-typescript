a8e6c724498b9462cbb5152a038a64d542e3c8d2
Hung Nguyen <nthung.vlvn@gmail.com>
2021-02-23 21:34:07 +0700

    array type

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,14 @@
 const person = {
   name: 'Hung',
   age: 28,
+  hobbies: ['Sports', 'Cooking', 'Hiking'],
 };
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sport'];
+
+for (let hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
 
 console.log(person.name);
