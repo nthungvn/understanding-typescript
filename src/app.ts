332a61dbeed1dc44a2f165fe99da718990575d61
Hung Nguyen <nthung.vlvn@gmail.com>
2021-03-07 08:46:27 +0700

    tuple type

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 // const person: { name: string; age: number } = {
-const person = {
+const person: {
+  name: string,
+  age: number,
+  hobbies: string[],
+  role: [number, string],
+} = {
   name: 'Hung',
   age: 28,
   hobbies: ['Sports', 'Cooking', 'Hiking'],
+  role: [1, 'admin'],
 };
 
 let favoriteActivities: string[];
@@ -13,3 +19,9 @@ for (let hobby of person.hobbies) {
 }
 
 console.log(person.name);
+
+person.role.push('employee');
+// person.role[0] = 'employee'; error
+person.role[0] = 2;
+person.role[1] = 'employee';
+console.dir(person);
