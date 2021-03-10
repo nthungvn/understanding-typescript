32498f8989871a9b460bf694b94eedb148775525
Hung Nguyen <nthung.vlvn@gmail.com>
2021-03-10 22:14:59 +0700

    setup code quality

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 const button = document.querySelector('button');
 
+function add(num1: number, num2: number, type: string) {
+  if (type === 'number') {
+    return num1 + num2;
+  }
+  return;
+}
+
 button?.addEventListener('click', () => {
+  // const userName = 'Hung';
+
   console.log('Clicked');
 });
 
