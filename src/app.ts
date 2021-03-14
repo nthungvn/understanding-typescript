bbe2635737a36bc91ccb44fd4b3eeba4e9079a35
Hung Nguyen <nthung.vlvn@gmail.com>
2021-03-14 19:47:19 +0700

    spread operator

--- src/app.ts
+++ src/app.ts
@@ -23,21 +23,36 @@ console.log(userName);
 
 const add = (num1: number, num2: number = 3) => {
   return num1 + num2;
-}
+};
 
 const add1 = (num1: number = 3, num2: number) => num1 + num2;
 
 console.log(add(1, 29));
 console.log(add1(12, 29));
 
-const printOutput: (a: string | number) => void = value => console.log(value);
+const printOutput: (a: string | number) => void = (value) => console.log(value);
 
 printOutput('Hello world');
 
 const button = document.querySelector('button');
 if (button) {
-  button.addEventListener('click', event => console.log(event));
+  button.addEventListener('click', (event) => console.log(event));
 }
 
 printOutput(add(3));
 // printOutput(add1(3)); Error
+
+const hobbies = ['Cooking', 'Sports'];
+const activeHobbies = ['Hiking'];
+
+activeHobbies.push(...hobbies);
+
+console.log(...activeHobbies);
+
+const person = {
+  name: 'Hung',
+  age: 23,
+};
+
+const copiedPerson = { ...person };
+console.log(copiedPerson);
