bfa82e82e13a72cc59d5207aa5add243cd466aef
Hung Nguyen <nthung.vlvn@gmail.com>
2021-03-14 17:18:06 +0700

    let and const

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+const userName = 'Hung';
+
+let age = 28;
+
+age = 29;
+
+console.log(age);
+console.log(userName);
+
+// let result = 0;
+// function add(num1: number, num2: number) {
+//   result = num1 + num2;
+//   return result;
+// }
+
+// add(3, 5);
+
+if (age === 29) {
+  let result = 1;
+}
+
+console.log(result);
