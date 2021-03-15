b3b1e8ff547ae7126fdd3369e28b863ab09d0727
Hung Nguyen <nthung.vlvn@gmail.com>
2021-03-15 22:54:35 +0700

    interitance

--- src/app.ts
+++ src/app.ts
@@ -22,15 +22,41 @@ class Department {
   }
 }
 
-const accounting = new Department('d1', 'Accounting');
-console.log(accounting);
-accounting.describe();
-accounting.addEmployee('Hung');
-accounting.addEmployee('Fuok');
+class ITDepartment extends Department {
+  private admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+  }
+
+  addReport(report: string) {
+    this.reports.push(report);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment('d1', ['Hung', 'Nguyen']);
+console.log(it);
+it.describe();
+it.addEmployee('Hung');
+it.addEmployee('Fuok');
 // accounting.employees[2] = 'Tran';
 // accounting.name = 'New name';
-accounting.printEmployeeInformation();
+it.printEmployeeInformation();
 
+const accounting = new AccountingDepartment('d2', []);
+accounting.addReport('Something went wrong...');
+accounting.printReports();
+console.log(accounting);
 
 // const accountingCopied = {
 //   name: 'Dummy',
