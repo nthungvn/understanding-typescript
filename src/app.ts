6f893c4b64e0837ce6600367423a545d1c61aa14
Hung Nguyen <nthung.vlvn@gmail.com>
2021-03-15 20:41:31 +0700

    constructor function and this keyword

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,19 @@ class Department {
   constructor(name: string) {
     this.name = name;
   }
+
+  describe(this: Department) {
+    console.log('Department: ' + this.name);
+  }
 }
 
 const accounting = new Department('Accounting');
 console.log(accounting);
+accounting.describe();
+
+const accountingCopied = {
+  name: 'Dummy',
+  describe: accounting.describe,
+}
+
+accountingCopied.describe();
