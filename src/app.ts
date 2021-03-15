9a106564deb6e1b1110588cb0fd8ddaf7b98385a
Hung Nguyen <nthung.vlvn@gmail.com>
2021-03-15 20:54:51 +0700

    private and public access modifier

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,38 @@
 class Department {
-  name: string;
+  // name: string;
+  private employees: string[] = [];
 
-  constructor(name: string) {
-    this.name = name;
+  constructor(private name: string) {
+    // this.name = name;
   }
 
   describe(this: Department) {
     console.log('Department: ' + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 const accounting = new Department('Accounting');
 console.log(accounting);
 accounting.describe();
+accounting.addEmployee('Hung');
+accounting.addEmployee('Fuok');
+// accounting.employees[2] = 'Tran';
+// accounting.name = 'New name';
+accounting.printEmployeeInformation();
 
-const accountingCopied = {
-  name: 'Dummy',
-  describe: accounting.describe,
-}
 
-accountingCopied.describe();
+// const accountingCopied = {
+//   name: 'Dummy',
+//   describe: accounting.describe,
+// }
+
+// accountingCopied.describe();
