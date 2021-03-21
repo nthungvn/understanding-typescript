62e6b462e37fb038ab2226af1a2a6d1726b08475
Hung Nguyen <nthung.vlvn@gmail.com>
2021-03-21 09:09:30 +0700

    type guard

--- src/app.ts
+++ src/app.ts
@@ -18,10 +18,74 @@ e1 = {
   startDate: new Date(),
 };
 
-type Combinable = string | boolean;
+type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Numeric & Combinable;
 
 let value: Universal;
-value = false;
+value = 3;
+
+function add(n1: Combinable, n2: Combinable) {
+  if (typeof n1 === 'string' || typeof n2 === 'string') {
+    return +n1 + +n2;
+  }
+  return n1 + n2;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log(`Name: ${emp.name}`);
+  if ('privileges' in emp) {
+    console.log(`Privileges: ${emp.privileges}`);
+  }
+  if ('startDate' in emp) {
+    console.log(`startDate: ${emp.startDate}`);
+  }
+}
+
+const e2 = {
+  name: 'Phuoc',
+  privileges: ['create-server'],
+};
+
+const e3 = {
+  name: 'Tang',
+  startDate: new Date(),
+};
+
+printEmployeeInformation(e1);
+printEmployeeInformation(e2);
+printEmployeeInformation(e3);
+
+class Car {
+  drive() {
+    console.log('Driving a car...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading a cargo... ' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(3);
+  }
+  // if ('loadCargo' in vehicle) {
+  //   vehicle.loadCargo(3);
+  // }
+}
+
+useVehicle(new Car());
+useVehicle(new Truck());
