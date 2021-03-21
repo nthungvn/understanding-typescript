f3da7aad6caee3c3bbb1da33da462d08aa5aad17
Hung Nguyen <nthung.vlvn@gmail.com>
2021-03-21 16:38:12 +0700

    another generic function

--- src/app.ts
+++ src/app.ts
@@ -23,3 +23,22 @@ const mergeObj = merge({ name: 'Hung', hobbies: ['Sports'] }, { age: 28 });
 // );
 console.log(mergeObj);
 console.log(mergeObj.name);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = 'Got one element.';
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.';
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(''));
+console.log(countAndDescribe([]));
+console.log(countAndDescribe('Hi there!'));
+console.log(countAndDescribe(['Sports', 'Cooking']));
