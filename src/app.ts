2ddae3b83ad30bc49c8d99f1c800eb8d3589a66d
Hung Nguyen <nthung.vlvn@gmail.com>
2021-03-28 10:03:27 +0700

    interacting with DOM

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 class ProjectInput {
   private templateEl: HTMLTemplateElement;
   private hostEl: HTMLDivElement;
-  private el: HTMLFormElement;
+  private formEl: HTMLFormElement;
+
+  private titleInputEl: HTMLInputElement;
+  private descriptionInputEl: HTMLInputElement;
+  private peopleInputEl: HTMLInputElement;
 
   constructor() {
     this.templateEl = document.getElementById(
@@ -10,12 +14,29 @@ class ProjectInput {
     this.hostEl = document.getElementById('app') as HTMLDivElement;
 
     const importedNote = document.importNode(this.templateEl.content, true);
-    this.el = importedNote.firstElementChild as HTMLFormElement;
+    this.formEl = importedNote.firstElementChild as HTMLFormElement;
+    this.formEl.id = 'user-input';
+    this.titleInputEl = this.formEl.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputEl = this.formEl.querySelector('#description') as HTMLInputElement;
+    this.peopleInputEl = this.formEl.querySelector('#people') as HTMLInputElement;
+    this.configure();
     this.attach();
   }
 
+  @AutoBind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputEl.value);
+    console.log(this.descriptionInputEl.value);
+    console.log(this.peopleInputEl.value);
+  }
+
+  private configure() {
+    this.formEl.addEventListener('submit', this.submitHandler.bind(this))
+  }
+
   private attach() {
-    this.hostEl.insertAdjacentElement('afterbegin', this.el);
+    this.hostEl.insertAdjacentElement('afterbegin', this.formEl);
   }
 }
 const projectInput = new ProjectInput();
