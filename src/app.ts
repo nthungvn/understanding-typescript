157c4f06779d5b62404ed361548f629a54f19019
Hung Nguyen <nthung.vlvn@gmail.com>
2021-04-03 18:10:22 +0700

    configure webpack dev server

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 import { ProjectInput } from './components/project-input';
 import { ProjectList } from './components/project-list';
 
-namespace App {
-  new ProjectInput();
-  new ProjectList('active');
-  new ProjectList('finished');
-}
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
